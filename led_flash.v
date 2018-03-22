8573f0aeca0d50c992919637bb54d92441c26af0
wintermao <winter_mao@163.com>
2018-03-22 13:31:26 +0800

    1 新增led_flash模块，从led0输出闪灯，用于标识fpga工程运行 2 修改始终输入频率为32M，修改pll倍频为3倍，原设置为4倍，导致系统时钟实际为128M，是的nios无法正常运行

---
+++ led_flash.v
@@ -0,0 +1,32 @@
+module led_flash( in_clk,reset_n,flash);  
+   input in_clk;
+   input reset_n;
+   output flash;
+   
+   reg flash;
+   reg [23:0]counter_temp;
+
+parameter OSC_CLOCK=27000000;
+initial
+begin
+flash=0;
+counter_temp=0;
+end
+always @(posedge in_clk or negedge reset_n)
+begin
+	if (reset_n == 0)
+    begin
+    	counter_temp<=0;
+			flash<=0;
+    end
+  else
+    begin
+    	counter_temp=counter_temp+1;
+    	if(counter_temp>=(OSC_CLOCK/2))
+    	begin
+    		counter_temp=0;
+    		flash=~flash;
+    	end
+  	end
+ end
+endmodule
