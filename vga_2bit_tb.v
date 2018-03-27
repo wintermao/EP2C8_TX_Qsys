19abd1e38ac48e9c4b0a551cd5fdeff73738b044
wintermao <winter_mao@163.com>
2018-03-27 23:27:45 +0800

    1 增加vga_2bit_tb测试模块 2 修改8彩条、4灰阶计数错误，后续应使用==判断计数值，不应用<=或>=判断 3 删除counter计数器

--- vga_2bit_tb.v
+++ vga_2bit_tb.v
@@ -5,38 +5,44 @@
 //------------------------------------------------
 
  `timescale 1ns/1ns
- 
-  `define delay 10
-  `define addr_rx 			3'h0
-  `define addr_tx 			3'h1
-  `define addr_status 	3'h2
-  `define addr_control	3'h3
-  `define addr_divide 	3'h4
-  `define addr_chipsel 	3'h5
+ `define delay 12.5
  
  module vga_2bit_tb;
-  
- vga_2bit tb(clock,reset_n,Hs,Vs,Blank,R,G,B,SEL);
-
-	reg clock,SEL;						//定义输入时钟引脚
+ 	reg clk,SEL;						//定义输入时钟引脚
 	reg reset_n;
 	wire Hs,Vs,Blank;
-	wire [1:0] R,G,B;
+	wire [1:0] R,G,B; 
+ vga_2bit tb(clk,reset_n,Hs,Vs,Blank,R,G,B,SEL);
+
+
 
 	// Generates serial clock of time period 10
  initial                         
    begin
      clk = 0;
-     forever #5 clk = !clk;
+     forever #`delay clk = !clk;
    end
    
  initial                         
    begin
      reset_n = 1;
-     #5
+     #`delay
      reset_n = 0;
-     #5
+     #`delay
      reset_n = 1;
+     
+     #`delay
+     SEL = 0;
+     #`delay
+     SEL = 1;
+     #`delay
+     SEL = 0;
+     #`delay
+     SEL = 1;     
+     #`delay
+     SEL = 0;
+     #`delay
+     SEL = 1;
    end
 
 
